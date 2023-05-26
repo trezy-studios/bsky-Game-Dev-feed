333a3ec3362c781fa6ccc982ae239de25d5d96ac
Trezy <t@trezy.com>
2023-05-26 14:49:42 -0500

    use `PORT` if available

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ const run = async () => {
   const serviceDid =
     maybeStr(process.env.FEEDGEN_SERVICE_DID) ?? `did:web:${hostname}`
   const server = FeedGenerator.create({
-    port: maybeInt(process.env.FEEDGEN_PORT) ?? 3000,
+    port: maybeInt(process.env.FEEDGEN_PORT ?? process.env.PORT) ?? 3000,
     listenhost: maybeStr(process.env.FEEDGEN_LISTENHOST) ?? 'localhost',
     sqliteLocation: maybeStr(process.env.FEEDGEN_SQLITE_LOCATION) ?? ':memory:',
     subscriptionEndpoint:
